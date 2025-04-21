9a20ac70ccac22f1b59215622b89125b6f87772f
infosys <infosys@C4228>
2025-04-21 09:57:51 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter01/ProductSample/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ProductSample {
+    internal class Program {
+        static void Main(string[] args) {
+            Product karinto = new Product(123, "かりんとう", 180);
+
+            Product daihuku = new Product(124, "大福", 200);
+            //大福    
+            //税抜きの価格を表示
+
+            Console.WriteLine(daihuku.Name + "の税抜き価格は" + daihuku.Price + "です");
+
+            //消費税額の表示
+
+            Console.WriteLine(daihuku.Name + "の消費税価格は" + daihuku.GetTax() + "です");
+
+            //税込み価格の表示
+
+            Console.WriteLine(daihuku.Name + "の税込み価格は" + daihuku.GetPriceincledingTax() + "です");
+
+
+            //かりんとう
+            //税抜きの価格を表示
+
+            Console.WriteLine(karinto.Name + "の税抜き価格は" + karinto.Price + "です");
+
+            //消費税額の表示
+
+            Console.WriteLine(karinto.Name + "の消費税価格は" + karinto.GetTax() + "です");
+
+            //税込み価格の表示
+
+            Console.WriteLine(karinto.Name + "の税込み価格は" + karinto.GetPriceincledingTax() + "です");
+        }
+    }
+}
---
+++ Chapter02/DistanceConverter/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace DistanceConverter {
+    internal class Program {
+        static void Main(string[] args) {
+
+            //string から intに変換 
+            int start = int.Parse(args[1]);
+            int end = int.Parse(args[2]);
+
+            if(args.Length >= 1 && args[0] == "-tom") {
+                PrintFeetToMeterlist(start, end);
+            } else {
+                PrintFeetToMeterlist(start, end);
+            }
+
+            /*if (args.Length >= 1 && args[0] == "-tom") {
+                for (int feet = s; feet <= e; feet++) {
+                    //double meter = feet * 0.3048;
+                    double meter = FeetToMeter(feet);
+                    Console.WriteLine($"{feet}ft = {meter:0.0000}m");
+                }
+            } else if (args.Length >= 1 && args[0] == "-tof") {
+                for (int meter = s; meter <= e; meter++) {
+                    //double meter = feet * 0.3048;
+                    double feet = MeterToFeet(meter);
+                    Console.WriteLine($"{meter}m = {feet:0.0000}ft");
+                }
+            }*/
+        }
+        static void PrintFeetToMeterlist(int start ,int stop) {
+            for(int meter = start; meter <= stop; meter++) {
+                double feet = FeetToMeter(meter);
+                Console.WriteLine($"{meter}m = {feet:0.0000}ft");
+            }
+        }
+
+        static double FeetToMeter(int feet) {
+            return feet * 0.3048;
+        }
+
+        static double MeterToFeet(int meter) {
+            return meter / 0.3048;
+        }
+    }
+}
