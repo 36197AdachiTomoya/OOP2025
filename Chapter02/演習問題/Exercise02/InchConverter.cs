99d609b57e5699baf27e28f729f700dba82b7043
AdachiTomoya <oibc36197@ojs.ac.jp>
2025-04-22 11:44:17 +0900

    完成

---
+++ Chapter02/演習問題/Exercise02/InchConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02
+{
+    class InchConverter {
+        private const double ratio = 0.0254;
+        //インチからメートルを求める
+        public static double ToMeter(double inch) {
+            return inch * ratio;
+        }
+
+    }
+}
---
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            InchToMeter(1, 10);
+        }
+        static void InchToMeter(int s, int end) {
+            for (int start = s; start >= end; start++) {
+                double meter = InchConverter.ToMeter(start);
+                Console.WriteLine($"{start}inch = {meter:0.0000}m");
+            }
+        }
+    }
+}
