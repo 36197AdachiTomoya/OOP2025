86118ba73adbce1fa3792a7de6df0ba0a96a0878
AdachiTomoya <oibc36197@ojs.ac.jp>
2025-05-13 10:08:28 +0900

    2.1.2完成

--- Chapter02/演習問題/Exercise01/Song.cs
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercise01{
+namespace Exercise01 {
     //2.1.1
-    public class Song{
+    public class Song {
         string title;
         string artistName;
         int length;
+
+        //2.1.2
+        public Song(string titile, string artistName, int length) {
+
+        }
     }
 }
