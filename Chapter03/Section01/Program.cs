973c8dd1d46d3156497f48d78cf5e7c6dedbee16
AdachiTomoya <oibc36197@ojs.ac.jp>
2025-05-16 14:14:14 +0900

    P68まで

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -8,11 +8,11 @@
             var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
 
             
-            Console.WriteLine(Count( numbers, delegate(int n) { return n % 2 == 0; }));
+            Console.WriteLine(Count(numbers, n => n >= 5 && n < 10));
 
         }
 
-        static int Count(int[] numbers, Func<int,bool> judge) {
+        static int Count(int[] numbers, Predicate<int> judge) {
             var count = 0;
 
             foreach (var n in numbers) {
