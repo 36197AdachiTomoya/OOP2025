cb2a833273a6c8a6e7d2fac7f4708156fe3cc098
AdachiTomoya <oibc36197@ojs.ac.jp>
2025-05-16 14:02:31 +0900

    p65まで

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -2,21 +2,24 @@
     public class Program {
         static void Main(string[] args) {
             //2.1.3
-            var songs = new Song[] {
-                new Song("Let it be", "The Beatles", 243),
-                new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
-                new Song("Close To You", "Carpenters", 276),
-                new Song("Honesty", "Billy Joel", 231),
-                new Song("I Will Always Love You", "Whitney Houston", 273),
-            };
-
-            printSongs(songs);
+            for (int i = 0; i < 5; i++) {
+                Console.Write("曲名：");
+                string title = Console.ReadLine();
+                Console.Write("アーティスト名：");
+                string artistName = Console.ReadLine();
+                Console.Write("演奏時間（秒）：");
+                int length = int.Parse(Console.ReadLine());
+                var songs = new Song[] {
+                    new Song(title,artistName,length)
+                };
+                printSongs(songs);
+            }
         }
 
         //2.1.4
         private static void printSongs(Song[] songs) {
             // ヘッダーを表示
-            Console.WriteLine("{0,-30} {1,-20} {2,5}", "Title", "Artist", "Length");
+            Console.WriteLine("{0,-30} {1,-20} {2,5}", "Title", "Artist", "Time");
 
             foreach (var song in songs) {
                 TimeSpan time = TimeSpan.FromSeconds(song.Length);
---
+++ Chapter03/Section01/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Section01 {
+    internal class Program {
+
+        static void Main(string[] args) {
+            //Console.Write("カウントしたい数値：");
+            //int num = int.Parse(Console.ReadLine());
+
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+
+            
+            Console.WriteLine(Count( numbers, delegate(int n) { return n % 2 == 0; }));
+
+        }
+
+        static int Count(int[] numbers, Func<int,bool> judge) {
+            var count = 0;
+
+            foreach (var n in numbers) {
+                //引数で受け取ったメソッドを呼び出す
+                if ( judge(n) == true) {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+    }
+}
