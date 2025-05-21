906f1363292e9716e2c61350eeba1f5e5f07e342
AdachiTomoya <oibc36197@ojs.ac.jp>
2025-05-21 10:12:26 +0900

    一行に変更

--- Chapter03/演習問題/Exercise01/Program.cs
+++ Chapter03/演習問題/Exercise01/Program.cs
@@ -20,29 +20,33 @@
         }
 
         private static void Exercise1(List<int> numbers) {
-            var exists = numbers.Exists(n => n % 8 == 0 || n % 9 == 0);
-            if(exists) {
-                Console.WriteLine("存在します");
-            } else {
-                Console.WriteLine("存在しません");
-            }
+            Console.WriteLine(numbers.Exists(n => n % 8 == 0
+            || n % 9 == 0) ? "存在します" : "存在しません");
+            //var exists = numbers.Exists(n => n % 8 == 0 || n % 9 == 0);
+            //if(exists) {
+            //    Console.WriteLine("存在します");
+            //} else {
+            //    Console.WriteLine("存在しません");
+            //}
         }
         private static void Exercise2(List<int> numbers) {
             numbers.ForEach(s => Console.WriteLine(s / 2.0));
         }
 
         private static void Exercise3(List<int> numbers) {
-            var num = numbers.Where(s => s >= 50);
-            foreach(var s in num) {
-                Console.WriteLine(s);
-            }
+            numbers.Where(s => s >= 50).ToList().ForEach(s => Console.WriteLine(s));
+            //var num = numbers.Where(s => s >= 50);
+            //foreach (var s in num) {
+            //    Console.WriteLine(s);
+            //}
         }
 
         private static void Exercise4(List<int> numbers) {
-            List<int> num = numbers.Select(s => s * 2).ToList();
-            foreach(var s in num) {
-                Console.WriteLine(s);
-            }
+            numbers.Select(s => s * 2).ToList().ForEach(s => Console.WriteLine(s));
+            //var num = numbers.Select(s => s * 2).ToList();
+            //foreach (var s in num) {
+            //    Console.WriteLine(s);
+            //}
         }
     }
 }
