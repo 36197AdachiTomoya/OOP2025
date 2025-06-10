9f72ce25f0467bfdb52d947e8cd6f7d052549cfe
AdachiTomoya <oibc36197@ojs.ac.jp>
2025-06-10 10:08:17 +0900

    5.2.4完成

--- Chapter05/演習問題/Exercise02/Exercise02/Program.cs
+++ Chapter05/演習問題/Exercise02/Exercise02/Program.cs
@@ -1,6 +1,55 @@
-﻿namespace Exercise02 {
+﻿using Exercise01;
+
+namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
+            var ymCollection = new YearMonth[] {
+                new YearMonth(1980, 1),
+                new YearMonth(1990, 4),
+                new YearMonth(2000, 7),
+                new YearMonth(2010, 9),
+                new YearMonth(2024, 12),
+            };
+
+            Console.WriteLine("5.2.2");
+            Exercise2(ymCollection);
+
+            
+            
+
+            Console.WriteLine("5.2.4");
+            Exercise4(ymCollection);
+
+
+            Console.WriteLine("5.2.5");
+            Exercise5(ymCollection);
+        }
+
+        private static YearMonth FindFirst21C(YearMonth[] ymCollection) {
+            foreach (var s in ymCollection) {
+                if (s.Is21stCentury) {
+                    return s;
+                }
+            }
+            return null;
+        }
+
+        private static void Exercise2(YearMonth[] ymCollection) {
+            foreach (var s in ymCollection) {
+                Console.WriteLine(s);
+            }
+        }
+
+        private static void Exercise4(YearMonth[] ymCollection) {
+            if(FindFirst21C(ymCollection) == null) {
+                Console.WriteLine("21世紀のデータはありません");
+            } else {
+                Console.WriteLine(FindFirst21C(ymCollection).ToString().Substring(0, 4));
+            }
+            
+        }
+
+        private static void Exercise5(YearMonth[] ymCollection) {
             
         }
     }
