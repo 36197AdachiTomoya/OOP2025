5cf80a760c683184b7f3ef9524695164b2361b34
AdachiTomoya <oibc36197@ojs.ac.jp>
2025-06-10 11:57:37 +0900

    プライマリーコンストラクタ

--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -4,17 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercise01{
-    public record YearMonth{
+namespace Exercise01 {
+    public record YearMonth(int year, int month) {
 
         //5.1.1
-        public readonly int Year;
-        public readonly int Month;
+        public readonly int Year = year;
+        public readonly int Month = month;
 
-       public YearMonth(int year, int month) {
-            Year = year;
-            Month = month;
-        }
+        
 
         //5.1.2
         public bool Is21stCentury => Year >= 2001 && Year <= 2100;
@@ -22,7 +19,7 @@ namespace Exercise01{
         //5.1.3
         public YearMonth AddOneMonth() {
             YearMonth rtn;
-            if(Year == 12) {
+            if (Year == 12) {
                 rtn = new YearMonth(Year + 1, 1);
             } else {
                 rtn = new YearMonth(Year, Month + 1);
@@ -32,7 +29,7 @@ namespace Exercise01{
 
         //5.1.4
         public override string ToString() => Year + "年" + Month + "月";
-            
-        
+
+
     }
 }
