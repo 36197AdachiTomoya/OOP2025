d3dc4dc5e5baa056c3a592426210ad7624ae13e8
AdachiTomoya <oibc36197@ojs.ac.jp>
2025-06-17 10:33:34 +0900

    8.2.4

--- Chapter08/演習問題/Exercise02/Program.cs
+++ Chapter08/演習問題/Exercise02/Program.cs
@@ -8,6 +8,17 @@
             abbrs.Add("IOC", "国際オリンピック委員会");
             abbrs.Add("NPT", "核兵器不拡散条約");
 
+            //8.2.3
+            var count = abbrs.Count;
+
+            if (abbrs.Remove("NPT")) {
+                Console.WriteLine(abbrs.Count);
+            }
+            Console.WriteLine();
+
+            //8.2.4
+            var query = abbrs.GetAll().Where(s => s.Key.Length == 3);
+            
             // Getメソッドの利用例
             var names = new[] { "WHO", "FIFA", "NPT", };
             foreach (var name in names) {
