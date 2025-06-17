1371bc6a8ff7806b76ba3425c01759db0a8e9c10
AdachiTomoya <oibc36197@ojs.ac.jp>
2025-06-17 11:18:31 +0900

    設問＿１Studentクラス完成

---
+++ Test01/Test01/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test01 {
+    public class Student {
+        
+        public string Name { get; init; } = string.Empty;
+        public string Subject { get; init; } = string.Empty;
+        public int Score { get; init; }
+
+    }
+}
---
+++ Test01/Test01/StudentScore.cs
@@ -0,0 +1,9 @@
+﻿namespace Test01 {
+    internal class StudentScore {
+        private string v;
+
+        public StudentScore(string v) {
+            this.v = v;
+        }
+    }
+}
