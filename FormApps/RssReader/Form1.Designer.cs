c94eea8abdf06bbec42912be54a840af0ac29aea
AdachiTomoya <oibc36197@ojs.ac.jp>
2025-07-22 13:51:32 +0900

    お気に入り登録

--- FormApps/RssReader/Form1.Designer.cs
+++ FormApps/RssReader/Form1.Designer.cs
@@ -23,29 +23,35 @@
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            tbUrl = new TextBox();
+            cbUrl = new ComboBox();
             btRssGet = new Button();
             lbTitles = new ListBox();
+            wvRssLink = new Microsoft.Web.WebView2.WinForms.WebView2();
+            btBack = new Button();
+            btForward = new Button();
+            btfavorite = new Button();
+            ((System.ComponentModel.ISupportInitialize)wvRssLink).BeginInit();
             SuspendLayout();
             // 
-            // tbUrl
+            // cbUrl
             // 
-            tbUrl.Font = new Font("Yu Gothic UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 128);
-            tbUrl.Location = new Point(12, 24);
-            tbUrl.Name = "tbUrl";
-            tbUrl.Size = new Size(616, 33);
-            tbUrl.TabIndex = 0;
+            cbUrl.Font = new Font("Yu Gothic UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            cbUrl.Location = new Point(236, 24);
+            cbUrl.Name = "cbUrl";
+            cbUrl.Size = new Size(596, 33);
+            cbUrl.TabIndex = 0;
+           
             // 
             // btRssGet
             // 
             btRssGet.Font = new Font("Yu Gothic UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 128);
-            btRssGet.Location = new Point(657, 24);
+            btRssGet.Location = new Point(838, 24);
             btRssGet.Name = "btRssGet";
             btRssGet.Size = new Size(75, 33);
             btRssGet.TabIndex = 1;
             btRssGet.Text = "取得";
             btRssGet.UseVisualStyleBackColor = true;
-            btRssGet.Click += btRssGet_Click;
+            btRssGet.Click += btRssGet_ClickAsync;
             // 
             // lbTitles
             // 
@@ -55,27 +61,82 @@
             lbTitles.ItemHeight = 21;
             lbTitles.Location = new Point(12, 72);
             lbTitles.Name = "lbTitles";
-            lbTitles.Size = new Size(733, 508);
+            lbTitles.Size = new Size(400, 319);
             lbTitles.TabIndex = 2;
+            lbTitles.Click += lbTitles_Click;
+            // 
+            // wvRssLink
+            // 
+            wvRssLink.AllowExternalDrop = true;
+            wvRssLink.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            wvRssLink.CreationProperties = null;
+            wvRssLink.DefaultBackgroundColor = Color.White;
+            wvRssLink.Location = new Point(12, 402);
+            wvRssLink.Name = "wvRssLink";
+            wvRssLink.Size = new Size(400, 370);
+            wvRssLink.TabIndex = 4;
+            wvRssLink.ZoomFactor = 1D;
+            wvRssLink.NavigationCompleted += wvRssLink_NavigationCompleted;
+            // 
+            // btBack
+            // 
+            btBack.Font = new Font("Yu Gothic UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            btBack.Location = new Point(62, 23);
+            btBack.Name = "btBack";
+            btBack.Size = new Size(75, 33);
+            btBack.TabIndex = 5;
+            btBack.Text = "戻る";
+            btBack.UseVisualStyleBackColor = true;
+            btBack.Click += btBack_Click;
+            // 
+            // btForward
+            // 
+            btForward.Font = new Font("Yu Gothic UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            btForward.Location = new Point(143, 23);
+            btForward.Name = "btForward";
+            btForward.Size = new Size(75, 33);
+            btForward.TabIndex = 6;
+            btForward.Text = "進む";
+            btForward.UseVisualStyleBackColor = true;
+            btForward.Click += btForward_Click;
+            // 
+            // btfavorite
+            // 
+            btfavorite.Font = new Font("Yu Gothic UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            btfavorite.Location = new Point(919, 24);
+            btfavorite.Name = "btfavorite";
+            btfavorite.Size = new Size(102, 32);
+            btfavorite.TabIndex = 7;
+            btfavorite.Text = "お気に入り";
+            btfavorite.UseVisualStyleBackColor = true;
+            btfavorite.Click += btfavorite_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 614);
+            ClientSize = new Size(1056, 784);
+            Controls.Add(btfavorite);
+            Controls.Add(btForward);
+            Controls.Add(btBack);
+            Controls.Add(wvRssLink);
             Controls.Add(lbTitles);
             Controls.Add(btRssGet);
-            Controls.Add(tbUrl);
+            Controls.Add(cbUrl);
             Name = "Form1";
             Text = "RssReader";
+            ((System.ComponentModel.ISupportInitialize)wvRssLink).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
-        private TextBox tbUrl;
+        private ComboBox cbUrl;
         private Button btRssGet;
         private ListBox lbTitles;
+        private Microsoft.Web.WebView2.WinForms.WebView2 wvRssLink;
+        private Button btBack;
+        private Button btForward;
+        private Button btfavorite;
     }
 }
--- FormApps/RssReader/Form1.cs
+++ FormApps/RssReader/Form1.cs
@@ -10,20 +10,65 @@ namespace RssReader {
             InitializeComponent();
         }
 
-        private void btRssGet_Click(object sender, EventArgs e) {
-            using(var wc = new WebClient()) {
-                var url = wc.OpenRead(tbUrl.Text);
-                XDocument xdoc = XDocument.Load(url);  //RSSの取得
+        private async void btRssGet_ClickAsync(object sender, EventArgs e) {
+            using (var wc = new HttpClient()) {
+                using HttpResponseMessage response = await wc.GetAsync(cbUrl.Text);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                XDocument xdoc = XDocument.Parse(responseBody);  //RSSの取得
 
-                //RSSを解析して必要な要素を取得
-                items = xdoc.Root.Descendants("item").Select
-                    (x => new ItemData { Title = (string)x.Element("title"),}).ToList();
+                ////RSSを解析して必要な要素を取得
+                items = xdoc.Root.Descendants("item").Select(x =>
+                    new ItemData {
+                        Title = (string?)x.Element("title"),
+                        Link = (string?)x.Element("link"),
+                    }).ToList();
+
 
                 //リストボックスへタイトル表示
-                foreach(var s in items) {
-                    lbTitles.Items.Add(s.Title);
-                }
+                items.ForEach(s => lbTitles.Items.Add(s.Title ?? "データなし"));
 
+
+            }
+        }
+
+        //タイトルを選択したときに呼ばれるイベントハンドラ
+        private void lbTitles_Click(object sender, EventArgs e) {
+            if (lbTitles.SelectedIndex >= 0 && lbTitles.SelectedIndex < items.Count) {
+                // インデックスが有効な範囲であることを確認
+                wvRssLink.Source = new Uri(items[lbTitles.SelectedIndex].Link);
+            }
+            //wvRssLink.Source = new Uri(items[lbTitles.SelectedIndex].Link);
+        }
+
+        //進む
+        private void btForward_Click(object sender, EventArgs e) {
+            wvRssLink.GoForward();  // ページを進める
+        }
+
+        //戻る
+        private void btBack_Click(object sender, EventArgs e) {
+            wvRssLink.GoBack();  // ページを戻す
+        }
+
+        private void wvRssLink_NavigationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e) {
+            GoForwardBtEnableSet();
+        }
+
+        private void GoForwardBtEnableSet() {
+            btBack.Enabled = wvRssLink.CanGoBack;
+            btForward.Enabled = wvRssLink.CanGoForward;
+        }
+
+        
+
+        private void btfavorite_Click(object sender, EventArgs e) {
+            if(cbUrl.Text == null) {
+                return;
+            } else if(cbUrl.Items.Contains(cbUrl.Text)){
+                return;
+            } else {
+                cbUrl.Items.Add(cbUrl.Text);
             }
         }
     }
