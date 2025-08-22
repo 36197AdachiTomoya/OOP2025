455f7d3655d4aca1acc175a11839e8a391b4571a
AdachiTomoya <oibc36197@ojs.ac.jp>
2025-08-22 15:07:15 +0900

    演習問題

---
+++ Chapter11/Section03/Section03/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+namespace Section03 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "oijefwoiワヲン";
+
+            Match match = Regex.Match(text, @"\p{IsKatakana}+");
+            if (match.Success) {
+                Console.WriteLine($"{match.Index}, {match.Value}");
+            }
+            var matches = Regex.Matches(text, @"\p{IsKatakana}+");
+            foreach(Match match2 in matches) {
+                Console.WriteLine($"Index={match.Index},Length={match.Length},Value{match.Value}");
+            }
+        }
+    }
+}
