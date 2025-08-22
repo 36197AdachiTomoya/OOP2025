197e766d566e7fb4938968f09b8df8cf9c56224f
AdachiTomoya <oibc36197@ojs.ac.jp>
2025-08-22 15:57:53 +0900

    Section03

--- Chapter11/Section03/Section03/Program.cs
+++ Chapter11/Section03/Section03/Program.cs
@@ -12,6 +12,23 @@ namespace Section03 {
             foreach(Match match2 in matches) {
                 Console.WriteLine($"Index={match.Index},Length={match.Length},Value{match.Value}");
             }
+
+            var text2 = "private List<string> results = new List<string>();";
+            var matches2 = Regex
+                .Matches(text2, @"\b[a-z]+\b")
+                .Cast<Match>()
+                .OrderBy(x => x.Length);
+            foreach(Match match3 in matches2) {
+                Console.WriteLine
+                    ($"Index={match3.Index},Length={match3.Length},Value={match3.Value}");
+            }
+
+
+
+
+
+
+
         }
     }
 }
