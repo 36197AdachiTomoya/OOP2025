6e89777ec62d3b0a3b6dc6db3bb17c7b35318bf4
AdachiTomoya <oibc36197@ojs.ac.jp>
2025-08-22 15:44:58 +0900

    演習問題Exercise03

---
+++ Chapter11/演習問題/Exercise03/Exercise03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            string[] texts = [
+                "Time is money.",
+                "What time is it?",
+                "It will take time.",
+                "We reorganized the timetable.",
+            ];
+
+            foreach(var line in texts) {
+                var regex = new Regex(@"\btime\b",RegexOptions.IgnoreCase);
+                var match = regex.Match(line);
+                if (match.Success) {
+                    Console.WriteLine($"{line},{match.Index}");
+                }
+            }
+
+        }
+    }
+}
