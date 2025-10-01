fa693f6c50f277366a172f7b74183d5540e9f6aa
AdachiTomoya <oibc36197@ojs.ac.jp>
2025-10-01 12:10:20 +0900

    途中

--- WPF/ConverterApp/MainWindow.xaml.cs
+++ WPF/ConverterApp/MainWindow.xaml.cs
@@ -20,6 +20,44 @@ namespace ConverterApp {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            string[] items = { "mm", "cm", "m", "km" };
+            foreach (var item in items) {
+                MetricUnit.Items.Add(item);
+                ImperialUnit.Items.Add(item);
+            }
+        }
+
+        private void ImperialUnitToMetric_Click(object sender, RoutedEventArgs e) {
+            double result = 0;
+            double i;
+            string imperialUnit = ImperialUnit.SelectedItem.ToString();
+
+            string metricUnit = MetricUnit.SelectedItem.ToString();
+            if (double.TryParse(ImperialValue.Text,out i)) {
+                if (ImperialUnit.Items.Equals("km")) {
+                    if (metricUnit == "cm") result = i / 10;
+                    else if (metricUnit == "m") result = i / 1000;
+                    else if (metricUnit == "km") result = i / 1000000;
+                    MetricUnit.Text = result.ToString();
+                } else if (ImperialUnit.Items.Equals("m")) {
+                    if (metricUnit == "cm") result = i / 10;
+                    else if (metricUnit == "m") result = i / 1000;
+                    else if (metricUnit == "km") result = i / 1000000;
+                    MetricUnit.Text = result.ToString();
+                } else if (ImperialUnit.Items.Equals("cm")) {
+                    if (metricUnit == "cm") result = i / 10;
+                    else if (metricUnit == "m") result = i / 1000;
+                    else if (metricUnit == "km") result = i / 1000000;
+                    MetricUnit.Text = result.ToString();
+                } else {
+                    if (metricUnit == "cm") result = i / 10;
+                    else if (metricUnit == "m") result = i / 1000;
+                    else if (metricUnit == "km") result = i / 1000000;
+                    MetricUnit.Text = result.ToString();
+                }
+            } else {
+                MessageBox.Show("数値を入力してください");
+            }
         }
     }
 }
