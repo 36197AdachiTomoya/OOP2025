87cf6ac9fe8c02c3d78383df8e7f8676d79ada4e
AdachiTomoya <oibc36197@ojs.ac.jp>
2025-10-09 13:37:54 +0900

    単位変換アプリ

---
+++ WPF/PrismObservesSample/MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PrismObservesSample {
+    class MainWindowViewModel : BindableBase {
+        private string _input1 = "";
+        public string Input1 {
+            get => _input1;
+            set => SetProperty(ref _input1,value);
+        }
+
+        private string _input2 = "";
+        public string Input2 {
+            get => _input2;
+            set => SetProperty(ref _input2,value);
+        }
+
+        private string _result = "";
+        public string Result {
+            get => _result;
+            set => SetProperty(ref _result, value);
+        }
+
+        //コンストラクタ
+        public MainWindowViewModel() {
+            SumCommand = new DelegateCommand(ExcuteSum,canExcuteSum);
+        }
+        public DelegateCommand SumCommand { get; }
+
+        //足し算の処理
+        private void ExcuteSum() {
+            Result = (int.Parse(_input1) + int.Parse(_input2)).ToString();
+        }
+
+        private bool canExcuteSum() {
+            if (int.TryParse(Input1, out _) && int.TryParse(Input2, out _)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
