5e1ac1c4d3d368a33fd8e76ea205a4ca31ec49b0
AdachiTomoya <oibc36197@ojs.ac.jp>
2025-10-22 11:50:23 +0900

    CustomerApp完成　更新ボタンの仕様変更

--- SQLite/CustomerApp/CustomerApp/MainWindow.xaml.cs
+++ SQLite/CustomerApp/CustomerApp/MainWindow.xaml.cs
@@ -24,6 +24,29 @@ namespace CustomerApp {
         public MainWindow() {
             InitializeComponent();
             CustomerListView.ItemsSource = _customer;
+            CustomerListView.SelectionChanged += CustomerListView_SelectionChanged;
+        }
+
+        private void CustomerListView_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            var selectedCustomer = CustomerListView.SelectedItem as Customer;
+            if (selectedCustomer != null) {
+                NameTextBox.Text = selectedCustomer.Name;
+                PhoneTextBox.Text = selectedCustomer.Phone;
+                AddressTextBox.Text = selectedCustomer.Address;
+
+                if (selectedCustomer.Image != null) {
+                    using (var ms = new MemoryStream(selectedCustomer.Image)) {
+                        var bitmap = new BitmapImage();
+                        bitmap.BeginInit();
+                        bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                        bitmap.StreamSource = ms;
+                        bitmap.EndInit();
+                        CustomerImage.Source = bitmap;
+                    }
+                } else {
+                    CustomerImage.Source = null;  // 画像なしの場合はクリア
+                }
+            }
         }
 
         private void SaveButton_Click(object sender, RoutedEventArgs e) {
@@ -69,6 +92,35 @@ namespace CustomerApp {
 
 
         private void RefreshButton_Click(object sender, RoutedEventArgs e) {
+            var selectedCustomer = CustomerListView.SelectedItem as Customer;
+            if (selectedCustomer == null) {
+                MessageBox.Show("更新する顧客を選択してください。");
+                return;
+            }
+
+            byte[] imageBytes = null;
+
+            if (CustomerImage.Source is BitmapImage bitmapImage) {
+                using (MemoryStream ms = new MemoryStream()) {
+                    BitmapEncoder encoder = new PngBitmapEncoder();
+                    encoder.Frames.Add(BitmapFrame.Create(bitmapImage));
+                    encoder.Save(ms);
+                    imageBytes = ms.ToArray();
+                }
+            }
+
+            // 選択中の顧客情報を更新
+            selectedCustomer.Name = NameTextBox.Text;
+            selectedCustomer.Phone = PhoneTextBox.Text;
+            selectedCustomer.Address = AddressTextBox.Text;
+            selectedCustomer.Image = imageBytes;
+
+            using (var connection = new SQLiteConnection(App.databasePath)) {
+                connection.CreateTable<Customer>();
+                connection.Update(selectedCustomer);
+            }
+
+            // ListViewを更新表示
             RefreshCustomerList();
         }
 
