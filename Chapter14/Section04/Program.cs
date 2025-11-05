45f58f3ea77789dfc83ed0dfc282a8daeec847a9
AdachiTomoya <oibc36197@ojs.ac.jp>
2025-11-05 11:17:01 +0900

    コード14_13

---
+++ Chapter14/Section04/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace Section04 {
+    internal class Program {
+        static async Task Main(string[] args) {
+            HttpClient client = new HttpClient();
+            await GethtmlExample(client);
+        }
+
+        static async Task GethtmlExample(HttpClient httpClient) {
+            var url = "https://www.yahoo.co.jp";
+            var text = await httpClient.GetStringAsync(url);
+            Console.WriteLine(text);
+        }
+    }
+}
