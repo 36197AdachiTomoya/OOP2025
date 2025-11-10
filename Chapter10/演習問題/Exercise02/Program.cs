bdeb0e0333d476baff6ef50e6125a16979dba73b
AdachiTomoya <oibc36197@ojs.ac.jp>
2025-11-10 15:09:23 +0900

    10.2完成

---
+++ Chapter10/演習問題/Exercise02/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var filePath = "source.txt";
+            var lines = File.ReadLines(filePath)
+                        .Select((s,ix) => $"{ix + 1,4}:{s}");
+
+            var filPath2 = "newsource.txt";
+            using(var writer = new StreamWriter(filPath2)) {
+                foreach (var line in lines) {
+                    writer.WriteLine(line);
+                }
+            }
+        }
+    }
+}
