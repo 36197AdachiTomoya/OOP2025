23cb8da96a2a66e0a64c43ded7bbc9e2c5ecfcb5
AdachiTomoya <oibc36197@ojs.ac.jp>
2025-11-10 15:30:30 +0900

    10.3完成

---
+++ Chapter10/演習問題/Exercise03/Exercise03/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            var filePath = Console.ReadLine();
+            var filePath2 = Console.ReadLine();
+
+            var lines = File.ReadAllLines(filePath2);
+
+            using (var writer = new StreamWriter(filePath, append : true)) {
+                foreach (var line in  lines) {
+                    writer.WriteLine(line);
+                }
+            }
+        }
+    }
+}
