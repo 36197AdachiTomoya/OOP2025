7381e844c722d7722b954ae58ba340cd6d4b2912
AdachiTomoya <oibc36197@ojs.ac.jp>
2025-11-10 14:07:49 +0900

    10.1.1完成

---
+++ Chapter10/演習問題/CountClass/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CountClass {
+    internal class Program {
+        static void Main(string[] args) {
+            string filePath = "source.txt";
+            using (StreamReader reader = new StreamReader(filePath)) {
+                string? line;
+                int cnt = 0;
+                while((line = reader.ReadLine()) != null) {
+                    string[] text = line.Split(' ');
+                    int c = text.ToList().Where(s => s.Trim() == "class").Count();
+                    if(c > 0) {
+                        cnt++;
+                    }
+                }
+                Console.WriteLine(cnt);
+            }
+        }
+    }
+}
