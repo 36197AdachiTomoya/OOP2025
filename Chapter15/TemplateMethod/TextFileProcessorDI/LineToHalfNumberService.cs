4e5af7eda1eef1a19166af5374f2b85c17bdcf23
AdachiTomoya <oibc36197@ojs.ac.jp>
2025-11-19 09:58:48 +0900

    15.1完成

---
+++ Chapter15/TemplateMethod/TextFileProcessorDI/LineToHalfNumberService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileProcessorDI {
+    internal class LineToHalfNumberService : ITextFileService {
+        private int _count;
+        public void Initialize(string fname) {
+            _count = 0;
+        }
+
+        public void Execute(string line) {
+            _count++;
+            string str = line.Normalize(NormalizationForm.FormKD);
+            Console.WriteLine(str);
+        }
+
+        public void Terminate() {
+            
+        }
+    }
+}
--- Chapter15/TemplateMethod/TextFileProcessorDI/Program.cs
+++ Chapter15/TemplateMethod/TextFileProcessorDI/Program.cs
@@ -1,7 +1,7 @@
 ﻿namespace TextFileProcessorDI {
     internal class Program {
         static void Main(string[] args) {
-            var service = new LineOutputService();
+            var service = new LineToHalfNumberService();
             var processor = new TextFileProcessor(service);
             Console.Write("パスの入力：");
 
